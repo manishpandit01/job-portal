20fb411ae10a4e015d410fdc04e7758b57f550b6
Manish <manishpandit9867@gmail.com>
2026-04-29 22:54:43 +0545

    creating models

--- jobportal/models.py
+++ jobportal/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
 # Create your models here.
+class TimeStamp(models.Model):
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract=True
+        
+class Category(TimeStamp):
+    name=models.CharField(max_length=100,unique=True)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering=["name"]
+        verbose_name="category"
+        verbose_name_plural="Categories"
+        
+class Company(TimeStamp):
+    name=models.CharField(max_length=100)
+    description=models.TextField(blank=True)
+    website=models.URLField(blank=True)
+    location=models.CharField(max_length=150)
+    
+    def __str__(self):
+        return self.name
+        
+class JobPost(TimeStamp):
+    JOB_TYPE=(
+        ("full_time","Full Time"),
+        ("part_time","Part Time"),
+        ("internship","Internship"),
+        ("remote","Remote"),
+    )
+    
+    title=models.CharField(max_length=140)
+    logo=models.ImageField( upload_to="company_logo/",blank=False)
+    location=models.CharField(max_length=150)
+    job_type=models.CharField(max_length=100,choices=JOB_TYPE)
+    salary=models.CharField(max_length=100,blank=True)
+    description=models.TextField()
+    responsibility=models.TextField()
+    qualification=models.TextField()
+    summary=models.TextField(blank=True)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    company=models.ForeignKey(Company,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+    
+    
+    
+    
