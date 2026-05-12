8c73fa5625e608737bcf41ac916fca8a5b727c17
Manish <manishpandit9867@gmail.com>
2026-05-12 22:07:24 +0545

    Add unfold for admin panel

--- jobportal/admin.py
+++ jobportal/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
+from jobportal import forms
 from jobportal.models import Category,Company,JobApplication,JobPost,Contact,Testimonial
+from unfold.admin import ModelAdmin
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Company)
-admin.site.register(JobPost)
+#admin.site.register(JobPost)
 admin.site.register(JobApplication)
 admin.site.register(Testimonial)
 admin.site.register(Contact)
+
+@admin.register(JobPost)
+class JobAdmin(ModelAdmin):
+   list_dsiplay=("title","company","location")
